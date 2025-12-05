84ceb1f2a92aabf28a605f5b3c9354f91d8fbfdf
brandsagaceceo <brandsagaceceo@gmail.com>
2025-12-05 17:45:51 -0500

    Re-enable onboarding for role selection, add .npmrc for peer deps

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -20,7 +20,7 @@ export default function SignInPage() {
           routing="path"
           path="/sign-in"
           signUpUrl="/sign-up"
-          afterSignInUrl="/profile"
+          afterSignInUrl="/onboarding"
         />
       </div>
     </div>
